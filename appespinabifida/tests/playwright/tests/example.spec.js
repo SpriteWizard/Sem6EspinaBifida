bd85386c71c55ececf5b4b2fb60fd75cb0639c41
Willian Salomon Lemus Sanchez <willianlemus050506@gmail.com>
2026-05-21 20:02:07 -0600

    feat: add Playwright tests with Qase integration

--- appespinabifida/tests/playwright/tests/example.spec.js
+++
@@ -1,7 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Qase - Playwright Integration', async ({ page }) => {
-  await page.goto('https://qase.io/');
-  const title = await page.title();
-  expect(title).toBe('Qase | Test management software for quality assurance');
-});
